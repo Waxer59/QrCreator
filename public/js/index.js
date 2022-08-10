d77738880eff7485952295df82a9505ad9b28ef3
Waxer59 <hnboyano@gmail.com>
2022-08-10 09:05:26 +0200

    Añadida la funcionalidad de descargar el qr en formato svg

--- public/js/index.js
+++ public/js/index.js
@@ -3,8 +3,25 @@
 const submit = document.querySelector("#submit");
 const text = document.querySelector("#text");
 const qrCode = document.querySelector("#qr-code");
+const dwnBtn = document.querySelector('#dwn-btn');
 
 // Functions
+
+function download(filename, text) {
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 
 // Listeners
 
@@ -15,7 +32,7 @@ const checkText = () => {
   return false;
 };
 
-submit.addEventListener("click", async (e) => {
+submit.addEventListener("click", (e) => {
   e.preventDefault();
   if (checkText()) {
     alert("Please complete all the fields");
@@ -37,3 +54,27 @@ submit.addEventListener("click", async (e) => {
       qrCode.innerHTML = svg;
     });
 });
+
+dwnBtn.addEventListener('click', () =>{
+  if (checkText()) {
+    alert("Please complete all the fields");
+    return;
+  }
+  const req = {
+    method: "GET",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+      txt: `${text.value}`,
+    },
+  };
+
+  fetch("/api/createQr", req)
+    .then((response) => response.text())
+    .then((svg) => {
+      let filename = 'qr.svg';
+      download(filename,svg)
+    });
+})
