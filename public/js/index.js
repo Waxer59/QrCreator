02128801fde46dfa4dff9597049aec2ae714c173
Waxer59 <78129249+Waxer59@users.noreply.github.com>
2023-10-19 14:07:16 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,7 @@ const dwnBtn = document.querySelector('#dwn-btn');
 // Functions
 
 function download(filename, text) {
-  var element = document.createElement("a");
+  const element = document.createElement("a");
   element.setAttribute(
     "href",
     "data:text/plain;charset=utf-8," + encodeURIComponent(text)
